4bacf1fe4372915a8ae8ce5c763342c77fc3c614
Mihai Slujitoru <mihai@thecreativemomentum.com>
2016-11-08 09:04:42 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,76 @@
+<?php
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js", false, null);
+	wp_enqueue_script('jquery');
+}
+function _remove_script_version( $src ){
+	$parts = explode( '?', $src );
+	return $parts[0];
+}
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+remove_action('wp_head', 'wp_generator');
+
+// Add default posts and comments RSS feed links to head.
+add_theme_support( 'automatic-feed-links' );
+/*
+ * Let WordPress manage the document title.
+ * By adding theme support, we declare that this theme does not use a
+ * hard-coded <title> tag in the document head, and expect WordPress to
+ * provide it for us.
+ */
+add_theme_support( 'title-tag' );
+
+/*
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support( 'html5', array(
+	'search-form',
+	'comment-form',
+	'comment-list',
+	'gallery',
+	'caption',
+) );
+
+add_theme_support( 'menus' );
+if ( function_exists( 'register_nav_menus' ) ) {
+register_nav_menus(
+array(   
+	'main_nav' => 'Main Menu',
+	'footer_nav' => 'Footer Nav',
+	'social_nav' => 'Social Menu'
+));
+}
+
+
+function excerpt($limit) {
+	$excerpt = explode(' ', get_the_excerpt(), $limit);
+	if (count($excerpt)>=$limit) {
+		array_pop($excerpt);
+		$excerpt = implode(" ",$excerpt).'...';
+	} else {
+		$excerpt = implode(" ",$excerpt);
+	}
+	$excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+	return $excerpt;
+}
+function content($limit) {
+	$content = explode(' ', get_the_content(), $limit);
+	if (count($content)>=$limit) {
+		array_pop($content);
+		$content = implode(" ",$content).'...';
+	} else {
+		$content = implode(" ",$content);
+	}
+
+	$content = preg_replace('/\[.+\]/','', $content);
+	$content = apply_filters('the_content', $content);
+	$content = str_replace(']]>', ']]&gt;', $content);
+	return $content;
+}
+/*US IT THIS WAY <?php echo excerpt(25); ?> */
+?>
